198a7912303962a21a781cbb2763e87aeb6ef5ce
Grzegorz Głowacki <gglowacki93@gmail.com>
2017-12-29 17:02:57 +0100

    Dodano: Parsowanie JSON-a, stacje na mapie
    - Parsowanie JSONa ze strony https://api.citybik.es/v2/networks/bike_s-srm-szczecin
      z wykorzystaniem wątku w tle
    - Dodanie markerów stacji na mapie wraz z ich nazwą, ilością rowerów i wolnych miejsc

--- App/app/src/main/java/bike_s/arduino/bike_s/MainActivity.java
+++ App/app/src/main/java/bike_s/arduino/bike_s/MainActivity.java
@@ -1,7 +1,8 @@
 package bike_s.arduino.bike_s;
 
-import android.content.Context;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -16,12 +17,6 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -32,11 +27,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private String TAG = MainActivity.class.getSimpleName();
     private Session session;
+    GoogleMap mMap;
+    // URL to get contacts JSON
+    private static String url = "https://api.citybik.es/v2/networks/bike_s-srm-szczecin";
+    ArrayList<HashMap<String, String>> stationList;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
 
         session = new Session(this);
-        if(!session.loggedIn()){
+        if (!session.loggedIn()) {
             logout();
         }
 
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity
          */
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
-        mapFragment.getMapAsync( this);
+        mapFragment.getMapAsync(this);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -77,6 +81,8 @@ public class MainActivity extends AppCompatActivity
         TextView userNameTextView = (TextView) header.findViewById(R.id.userName);
         userNameTextView.setText(session.getUserName());
 
+        stationList = new ArrayList<>();
+        new GetStations().execute();
         /*
          * =============JSON z API
          * Jak na ten moment Fatal Error na API :|
@@ -84,7 +90,7 @@ public class MainActivity extends AppCompatActivity
          * https://api.citybik.es/v2/networks/bike_s-srm-szczecin
          * testuje na przykłądowym api
          */
-        RequestQueue queue = Volley.newRequestQueue(this);
+       /* RequestQueue queue = Volley.newRequestQueue(this);
         String url ="https://api.androidhive.info/contacts/";
         StringRequest stringRequest = new StringRequest( Request.Method.GET, url,
                 new Response.Listener<String>() {
@@ -117,6 +123,7 @@ public class MainActivity extends AppCompatActivity
         // Add the request to the RequestQueue.
         queue.add(stringRequest);
         //=============KONIEC JSONA z API
+        */
     }
 
     @Override
@@ -159,10 +166,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        switch(id)
-        {
-            case R.id.nav_stations:
-            {
+        switch (id) {
+            case R.id.nav_stations: {
                 Intent intent = new Intent(this, MapsActivity.class);
                 //EditText editText = (EditText) findViewById(R.id.editText);
                 //String message = editText.getText().toString();
@@ -170,8 +175,7 @@ public class MainActivity extends AppCompatActivity
                 startActivity(intent);
                 break;
             }
-            case R.id.btnLogout:
-            {
+            case R.id.btnLogout: {
                 logout();
                 break;
             }
@@ -183,14 +187,15 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+
     //Funkcja opowiedzialna za nadanie punktu na mapie
     public void onMapReady(GoogleMap googleMap) {
-        GoogleMap mMap = googleMap;
+        mMap = googleMap;
 
         LatLng zut = new LatLng(53.4475413, 14.4919891);
 
         mMap.addMarker(new MarkerOptions().position(zut).title("Marker ZUTu"));
-        mMap.moveCamera( CameraUpdateFactory.newLatLngZoom(zut,18));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zut, 18));
     }
 
     @Override
@@ -198,10 +203,123 @@ public class MainActivity extends AppCompatActivity
 
     }
 
-    private void logout(){
+    private void logout() {
         session.setLoggedIn(false, null);
         finish();
-        startActivity(new Intent(MainActivity.this,LoginActivity.class));
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 
+    private class GetStations extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            // Showing progress dialog
+            pDialog = new ProgressDialog(MainActivity.this);
+            pDialog.setMessage("Loading stations...");
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            JsonParser sh = new JsonParser();
+
+            // Making a request to url and getting response
+            String jsonStr = sh.makeServiceCall(url);
+
+            Log.e(TAG, "Response from url: " + jsonStr);
+
+            if (jsonStr != null) {
+                try {
+                    JSONObject jsonObj = new JSONObject(jsonStr);
+                    // Getting JSON Array node
+                    JSONObject network = jsonObj.getJSONObject("network");
+                    JSONArray stations = network.getJSONArray("stations");
+
+                    // looping through All Stations
+                    for (int i = 0; i < stations.length(); i++) {
+                        JSONObject c = stations.getJSONObject(i);
+                        getData(c);
+                    }
+                } catch (final JSONException e) {
+                    Log.e(TAG, "Json parsing error: " + e.getMessage());
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
+            } else {
+                Log.e(TAG, "Couldn't get json from server.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getApplicationContext(),
+                                "Couldn't get json from server.",
+                                Toast.LENGTH_LONG)
+                                .show();
+                    }
+                });
+            }
+            return null;
+        }
+
+        protected void getData(JSONObject c) throws JSONException {
+            String emptySlots = c.getString("empty_slots");
+            String freeBikes = c.getString("free_bikes");
+            String lat = c.getString("latitude");
+            String lng = c.getString("longitude");
+            String name = c.getString("name");
+
+            //JSONObject extra = c.getJSONObject("extra");
+            // String number = extra.getString("number");
+            //String slots = extra.getString("slots");
+            //String uid = extra.getString("uid");
+
+            // tmp hash map for single station
+            HashMap<String, String> station = new HashMap<>();
+
+            // adding each child node to HashMap key => value
+            //station.put("uid", uid);
+            station.put("name", name);
+            station.put("emptySlots", emptySlots);
+            station.put("freeBikes", freeBikes);
+            station.put("lat", lat);
+            station.put("lng", lng);
+            //station.put("number", number);
+            //station.put("slots", slots);
+
+            stationList.add(station);
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            // Dismiss the progress dialog
+            if (pDialog.isShowing())
+                pDialog.dismiss();
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            for (HashMap<String, String> map : stationList) {
+                double lat = Double.parseDouble(map.get("lat"));
+                double lng = Double.parseDouble(map.get("lng"));
+                int freeBikes = Integer.parseInt(map.get("freeBikes"));
+                int emptySlots = Integer.parseInt(map.get("emptySlots"));
+                String name = map.get("name");
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(lat, lng))
+                        .title(name)
+                        .snippet("Free bikes: " + freeBikes + " Empty slots: " + emptySlots));
+            }
+        }
+    }
 }
+
